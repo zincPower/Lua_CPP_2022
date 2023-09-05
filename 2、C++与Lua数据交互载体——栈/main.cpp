1966d40a14e0564927a1e061a8abbdaa3827f317
jiangpengyong <jiangpengyong@joyy.com>
2023-09-05 09:11:06 +0800

    feat: 增加数据交互

---
+++ 2、C++与Lua数据交互载体——栈/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 江澎涌 on 2023/1/21.
+//
+
+#include "lua.hpp"
+#include "stack.h"
+
+int main() {
+    printf("================= 栈 =================\n");
+    stackDemo();
+    printf("=============================================\n");
+
+    return 0;
+}
