e008162c2090e5a72b20ee12b365246418a0be7c
jiangpengyong <jiangpengyong@joyy.com>
2023-09-08 15:36:24 +0800

    feat: 栈操作

--- 2、C++与Lua数据交互载体——栈/stack.cpp
+++ 2、C++与Lua数据交互载体——栈/stack.cpp
@@ -48,6 +48,7 @@ void stackDemo() {
         printf("L 和 threadState 相同\n");
     }
 
+    printf("\n");
     printf("------ 栈中内容: ------\n");
     stackDump(L);
 
@@ -79,6 +80,7 @@ void stackDemo() {
     printf("lua_tothread(L, -1) == nullptr --> %d\n", lua_tothread(L, 1) == nullptr);
     printf("lua_topointer(L, -1) == nullptr --> %d\n", lua_topointer(L, 1) == nullptr);
 
+    printf("\n");
     printf("------ lua_len 的错误表现 ------\n");
     // 如果获取错误会抛出异常，PANIC: unprotected error in call to Lua API (attempt to get length of a nil value)
 //    lua_len(L, 1);
@@ -89,6 +91,7 @@ void stackDemo() {
     // 将长度弹出
     lua_pop(L, 1);
 
+    printf("\n");
     printf("------ 数值转换 ------\n");
     printf("lua_tonumber(L, -1) %f\n", lua_tonumber(L, -1));
     printf("lua_tointeger(L, -1) %llu\n", lua_tointeger(L, -1));
@@ -100,6 +103,7 @@ void stackDemo() {
     printf("lua_tonumberx(L, 2, &isNumber) %f\n", lua_tonumberx(L, 2, &isNumber));
     printf("%s 转 number 是否成功 %s\n", lua_tostring(L, 2), (isNumber == 0 ? "false" : "true"));
 
+    printf("\n");
     printf("------ lua_tolstring 和 lua_tostring ------\n");
     printf("lua_tolstring(L, -2, nullptr) --> %s\n", lua_tolstring(L, -2, nullptr));
     printf("lua_tostring(L, -2) --> %s\n", lua_tostring(L, -2));
@@ -109,46 +113,78 @@ void stackDemo() {
     printf("===================== 栈操作 =====================\n");
     printf("------ 栈中元素个数 ------\n");
     printf("lua_gettop(L) %d\n", lua_gettop(L));
+    // 清空栈
+    lua_settop(L, 0);
+    printf("lua_gettop(L) %d\n", lua_gettop(L));
 
-    printf("------ lua_pushvalue(-4): ------\n");
-    // 将栈顶第四位数压进栈顶，不会变动原来的数
-    lua_pushvalue(L, -4);
+    printf("\n");
+    printf("------ 构建测试数据的栈 ------\n");
+    lua_pushnumber(L, 10.8);
+    lua_pushstring(L, "jiang peng yong");
+    lua_pushnil(L);
+    lua_pushinteger(L, 29);
     stackDump(L);
 
-    printf("------ lua_replace(1): ------\n");
-    // 将栈顶的元素放置到指定的位置，并将栈顶元素弹出
-    lua_replace(L, 1);
+    printf("\n");
+    printf("------ lua_absindex ------\n");
+    printf("lua_absindex(L, -1) --> %d\n", lua_absindex(L, -1));
+    printf("lua_absindex(L, 1) --> %d\n", lua_absindex(L, 1));
+
+    printf("\n");
+    printf("------ lua_pushvalue(-3): ------\n");
+    // 将栈顶第二位的元素压进栈顶，不会变动原来的数
+    lua_pushvalue(L, -3);
     stackDump(L);
 
-    printf("------ lua_settop(5): ------\n");
-    // 设置栈长度，比原先长则用 nil 补，比原先短则截断
-    lua_settop(L, 5);
-    stackDump(L);
-
-    lua_settop(L, 6);
-    stackDump(L);
-
+    printf("\n");
     printf("------ lua_rotate(3, 1): ------\n");
     // 从指定索引的地方开始转动，n 为正数则往栈顶转，n 为负数则往栈底转
     lua_rotate(L, 3, 1);
     stackDump(L);
-//    lua_rotate(L, 3, -2);
-//    stackDump(L);
+    printf("------ lua_rotate(3, -2): ------\n");
+    lua_rotate(L, 3, -2);
+    stackDump(L);
+
+    printf("\n");
+    printf("------  lua_copy(L, 1, -1): ------\n");
+    lua_copy(L, 1, -1);
+    stackDump(L);
+
+    printf("\n");
+    printf("------ lua_insert(2): ------\n");
+    // 将栈顶元素移动到指定索引位置
+    lua_insert(L, 2);
+    stackDump(L);
 
     printf("------ lua_remove(-3): ------\n");
     // 删除指定索引
     lua_remove(L, -3);
     stackDump(L);
 
-    printf("------ lua_insert(2): ------\n");
-    // 将栈顶元素移动到指定索引位置
-    lua_insert(L, 2);
+    printf("\n");
+    printf("------ lua_replace(1): ------\n");
+    // 将栈顶的元素放置到指定的位置，并将栈顶元素弹出
+    lua_replace(L, 1);
     stackDump(L);
 
-    printf("------ lua_settop(-5): ------\n");
-    // 设置栈顶
-    lua_settop(L, -5);
-    printf("  \n");
+    printf("\n");
+    printf("------ lua_pop(2): ------\n");
+    lua_pop(L, 1);
+    stackDump(L);
+
+    printf("\n");
+    printf("------ lua_gettop(): ------\n");
+    printf("lua_gettop(L) --> %d\n", lua_gettop(L));
+
+    printf("\n");
+    printf("------ lua_settop(5): ------\n");
+    // 设置栈长度，比原先长则用 nil 补，比原先短则截断
+    lua_settop(L, 5);
+    stackDump(L);
+
+    printf("\n");
+    printf("------ lua_settop(3): ------\n");
+    lua_settop(L, 3);
     stackDump(L);
 
     lua_close(L);
