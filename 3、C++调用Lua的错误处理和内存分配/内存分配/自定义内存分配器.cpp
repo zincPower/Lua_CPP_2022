0d7b0af560deca583900253e91f0056fe7c191da
jiangpengyong <jiangpengyong@joyy.com>
2023-09-15 08:55:02 +0800

    feat: 内存分配函数

--- 3、C++调用Lua的错误处理和内存分配/内存分配/自定义内存分配器.cpp
+++ 3、C++调用Lua的错误处理和内存分配/内存分配/自定义内存分配器.cpp
@@ -14,6 +14,16 @@ static void *alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
         return realloc(ptr, nsize);
 }
 
+static void *newalloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+    const char *udContent = *(const char **) ud;
+    std::cout << "新的分配函数 " << "ud: " << udContent << "; *ptr: " << ptr << "; osize: " << osize << "; nsize: " << nsize << std::endl;
+    if (nsize == 0) {
+        free(ptr);
+        return nullptr;
+    } else
+        return realloc(ptr, nsize);
+}
+
 void allocationFunction() {
     // 常规的创建 lua_State
 //    auto L = luaL_newstate();
@@ -23,17 +33,17 @@ void allocationFunction() {
     auto ud = "ud test message.";
     auto L = lua_newstate(alloc, &ud);
 
-//    printf("------------- 获取 Lua 自带的内存分配函数 -------------\n");
-//    // lua_getallocf 第二个参数会返回之前设置的 自定义内存分配数据
-//    void *userData = nullptr;
-//    lua_Alloc allocFunc = lua_getallocf(L, &userData);
-//    const char *udContent = *(const char **) userData;
-//    std::cout << udContent << std::endl;
-//
-//    printf("------------- 设置 Lua 自带的内存分配函数 -------------\n");
-//    lua_setallocf(L, allocFunc, nullptr);
+    printf("------------- 获取 Lua 当前的内存分配函数 -------------\n");
+    // lua_getallocf 第二个参数会返回之前设置的 自定义内存分配数据
+    void *userData = nullptr;
+    lua_Alloc allocFunc = lua_getallocf(L, &userData);
+    const char *udContent = *(const char **) userData;
+    std::cout << &allocFunc << " " << udContent << std::endl;
+
+    printf("------------- 设置 Lua 的内存分配函数 -------------\n");
+    auto nud = "new ud test message.";
+    lua_setallocf(L, newalloc, &nud);
 
     std::cout << "关闭 lua_State " << std::endl;
     lua_close(L);
-
 }
