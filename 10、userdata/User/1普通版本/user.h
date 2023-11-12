d887b5987745c75a37dcdfd450f8a753b9cf9338
jiangpengyong <jiangpengyong@joyy.com>
2023-11-12 21:08:33 +0800

    feat: 整理 userdata - 增加 user 事例 - 整理 array 事例

---
+++ 10、userdata/User/1普通版本/user.h
@@ -0,0 +1,16 @@
+//
+// Created by 江澎涌 on 2022/5/8.
+//
+
+#ifndef CPP2022_USER_H
+#define CPP2022_USER_H
+
+#include <sstream>
+#include <limits>
+#include <string>
+#include "lua.hpp"
+#include "../../../config/env_config.h"
+
+void userSimpleDemo();
+
+#endif //CPP2022_USER_H
---
+++ 10、userdata/array/2增加元表/array.h
@@ -0,0 +1,15 @@
+//
+// Created by 江澎涌 on 2022/5/8.
+//
+
+#ifndef CPP2022_ARRAY_METATABLE_H
+#define CPP2022_ARRAY_METATABLE_H
+
+#include <limits>
+#include <string>
+#include "lua.hpp"
+#include "../../../config/env_config.h"
+
+void arrayUseMetatableDemo();
+
+#endif //CPP2022_ARRAY_METATABLE_H
